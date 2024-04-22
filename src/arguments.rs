7f232bf5fe193ee8af036afca1863265538bebdd
Brian Silver <github@briansilver.net>
2024-04-22 10:55:45 -0400

    more work around project settings and arguments

--- src/arguments.rs
+++ src/arguments.rs
@@ -1,9 +1,12 @@
 use clap::Parser;
 use std::path::PathBuf;
 
+use crate::project::{CStandard, CXXStandard};
+
 #[derive(Debug, Parser)]
 #[clap(version)]
 pub struct Arguments {
+    // project data
     #[arg(help = "Path to generate a project into. Also used to determine project name")]
     pub path: PathBuf,
 
@@ -13,6 +16,7 @@ pub struct Arguments {
     #[arg(long, help = "Default branch name", default_value = "main")]
     pub git_branch: Option<String>,
 
+    // project type
     #[arg(long = "exe", help = "Generate an executable")]
     pub executable: bool,
 
@@ -21,4 +25,22 @@ pub struct Arguments {
 
     #[arg(long = "header-lib", help = "Generate an header-only library")]
     pub header_libray: bool,
+
+    // C features
+    #[arg(long, help = "C Standard")]
+    pub c_std: Option<CStandard>,
+
+    // CXX features
+    #[arg(long, help = "C Standard")]
+    pub cxx_std: Option<CXXStandard>,
+
+    // extra tools
+    // FIX: should these be --enable-feat flags or --disable-feat or --no-feat flags?
+    // mixing and matching these based off personal preference may be confusing
+    // these should match project settings defaults
+    #[arg(long, help = "Enable clang-tidy")]
+    pub clang_tidy: bool,
+
+    #[arg(long, help = "Enable cppcheck")]
+    pub cppcheck: bool,
 }
--- src/project.rs
+++ src/project.rs
@@ -1,4 +1,7 @@
+use std::fmt::Display;
+
 use anyhow::{anyhow, Result};
+use clap::ValueEnum;
 use serde::Serialize;
 
 use crate::arguments::Arguments;
@@ -33,22 +36,34 @@ pub enum BenchmarkLibrary {
     // GoogleBenchmark,
 }
 
-#[derive(Debug, Clone, Copy, Serialize)]
+#[derive(Debug, Clone, Copy, Serialize, ValueEnum)]
 pub enum CXXStandard {
     CXX11,
-    // CXX14,
-    // CXX17,
-    // CXX20,
-    // CXX23,
+    CXX14,
+    CXX17,
+    CXX20,
+    CXX23,
 }
 
-#[derive(Debug, Clone, Copy, Serialize)]
+impl Display for CXXStandard {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+#[derive(Debug, Clone, Copy, Serialize, ValueEnum)]
 pub enum CStandard {
-    // C90,
+    C90,
     C99,
-    // C11,
-    // C17,
-    // C23,
+    C11,
+    C17,
+    C23,
+}
+
+impl Display for CStandard {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 #[derive(Debug, Serialize)]
@@ -74,10 +89,26 @@ pub struct Project {
 
 impl Project {
     pub fn new(args: Arguments) -> Self {
-        Self {
+        let mut s = Self {
             name: args.name.unwrap_or_default(),
             ..Default::default()
+        };
+
+        if let Some(c_std) = args.c_std {
+            s.c_standard = c_std;
         }
+        if let Some(cxx_std) = args.cxx_std {
+            s.cxx_standard = cxx_std;
+        }
+
+        if args.clang_tidy {
+            s.use_clang_tidy = true;
+        }
+        if args.cppcheck {
+            s.use_cppcheck = true;
+        }
+
+        s
     }
 
     // pub fn name(&self) -> &str {
@@ -111,8 +142,13 @@ impl Project {
             _ => return Err(anyhow!("mismatched project type input")),
         };
 
-        self.use_clang_tidy = inquire::Confirm::new("Add clang-tidy").prompt()?;
-        self.use_cppcheck = inquire::Confirm::new("Add cppcheck").prompt()?;
+        self.use_clang_tidy = inquire::Confirm::new("Add clang-tidy")
+            .with_default(self.use_clang_tidy)
+            .prompt()?;
+
+        self.use_cppcheck = inquire::Confirm::new("Add cppcheck")
+            .with_default(self.use_cppcheck)
+            .prompt()?;
 
         Ok(())
     }
@@ -139,6 +175,7 @@ impl Project {
 
 impl Default for Project {
     fn default() -> Self {
+        // FIX: these defaults should match cli flags and settings
         Self {
             name: "".into(),
             // version: Version::new(0, 1, 0),
